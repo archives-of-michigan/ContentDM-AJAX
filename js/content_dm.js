ab4aa8cd621147f4ee4d1d196632cdd69f9c8be2
Derek Kastner <dkastner@gmail.com>
2008-09-19 22:19:07 -0400

    abstracted ajax requests with callMethod, completed merge function

--- js/content_dm.js
+++ js/content_dm.js
@@ -1,38 +1,79 @@
-function ContentDM(host, root){
+//document.domain = environment.host; //'haldigitalcollections.cdmhost.com'; //environment.host; 
+
+function ContentDM(host, root) {
 	this.host = host;
 	this.root = root;
-	this.url = 'http://' + this.host + this.root + '/cdm_ajax/get.php';
-}  
 
+	this.last_method_result = null;
+	this.method_status = false;
+}
 ContentDM.prototype = {
-	collectionList: function(handler) {
-		jQuery.ajax({ 
-			dataType: "json",
-			type: 'get',
-			success: handler,
-			url: this.url,
+
+	callMethod: function(handler, failure, options) {
+		var onFail = failure ||
+			function(){
+				alert("Could not fetch list of collections from the server.  Please contact an administrator.");
+			};
+		var onSuccess = handler ||
+			function(data, status) {
+				$('#cdm').innerHtml = data;
+			};
+		
+		if(environment.config.test_stubs)
+			options = this.merge(options, { test_stubs: true });
+			
+		jQuery.ajax(this.merge(options,
+			{ 
+				dataType: 'jsonp',
+				type: 'post',
+				success: onSuccess,
+				error: onFail,
+				url: this.url(),
+				cache: true,
+				async: true  // TODO (handler != null)
+		}));
+		
+		//TODO - return results immediately (block and wait for results when async: false)
+		//if(handler) {
+		//} else {
+		//	return new Result(this.results, this.status);
+		//}
+		return true;
+	},
+
+	collectionList: function(handler, failure) {
+		return this.callMethod(handler, failure, { 
 			data: {
-				method: 'dmGetCollectionList'
+				command: 'dmGetCollectionList'
 			}
 		});
 	},
-	
-	query: function(options) {
-		jQuery.ajax({ 
-			dataType: "json",
-			type: 'get',
-			success: handler,
-			url: this.url,
-			data: this.merge({ method: 'dmQuery' }, options)
-		});
+
+	query: function(options, handler, failure) {
+		return this.callMethod(handler, failure, this.merge(options, { 
+			data: {
+				command: 'dmQuery'
+			}
+		}));
 	},
-	
-	
+
+
 	merge: function(a, b) {
-		var x = [1, 2, 3];
+		var merged = a;
 		jQuery.each(b, function(key, value) {
-			if(a[key] == undefined)
-		  	a[key] = value;
+			if(merged[key] == undefined)
+		  	merged[key] = value;
 		});
+		return merged;
+	},
+
+
+	url: function() {
+		return 'http://' + environment.config.host + environment.config.path;
 	}
-}
+};
+
+ContentDM.prototype.Result = function(data, status){
+	this.data = data;
+	this.status = status;
+};
