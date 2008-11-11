f7024bb77a07701b29058edd68b311d9f3698fbf
Derek Kastner <dkastner@gmail.com>
2008-11-10 22:22:12 -0500

    moved everything into cdm_ajax

---
+++ cdm_ajax/get.php
@@ -0,0 +1,33 @@
+<?
+include('./JSON.php');
+
+$command = $_POST['command'];
+
+$json = new Services_JSON();
+$params = $json->decode(stripslashes($_POST['params']));
+
+if($params->test_stubs) {
+	include('./stubs.php');
+}
+
+$results = '';
+switch($command) {
+	case 'dmQuery':
+		$total = 0;
+		$results = dmQuery($params['alias'], 
+											$params['searchstring'], 
+											$params['field'],
+											$params['sortby'],
+											$params['maxrecs'],
+											$params['start'],
+											$total);
+		break;
+	case 'dmGetCollectionList':
+		$results = dmGetCollectionList();
+		break;
+	default:
+		$results = array( 'error' => 'invalid operation requested: '.$_POST['command'] );
+}
+
+echo $json->encode($results);
+?>
