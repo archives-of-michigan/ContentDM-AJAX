809ad39f87caafa04e5806b9bcd3aa8f30103208
Derek Kastner <dkastner@gmail.com>
2008-11-18 20:39:44 -0500

    creating functions for discover collections

--- cdm_server.php
+++ cdm_server.php
@@ -2,6 +2,7 @@
 include('../dmscripts/DMSystem.php');
 include('config.php');
 include('JSON.php');
+include('airbag_functions.php')
 
 $command = $_POST['command'];
 
@@ -9,26 +10,32 @@ $json = new Services_JSON();
 $params = $json->decode(stripslashes($_POST['params']));
 
 if($params->test_stubs) {
-	include('stubs.php');
+  include('stubs.php');
 }
 
 $results = '';
 switch($command) {
-	case 'dmQuery':
-		$total = 0;
-		$results = dmQuery($params['alias'], 
-											$params['searchstring'], 
-											$params['field'],
-											$params['sortby'],
-											$params['maxrecs'],
-											$params['start'],
-											$total);
-		break;
-	case 'dmGetCollectionList':
-		$results = dmGetCollectionList();
-		break;
-	default:
-		$results = array( 'error' => 'invalid operation requested: '.$_POST['command'] );
+  case 'dmQuery':
+    $total = 0;
+    $results = dmQuery($params['alias'], 
+                      $params['searchstring'], 
+                      $params['field'],
+                      $params['sortby'],
+                      $params['maxrecs'],
+                      $params['start'],
+                      $total);
+    break;
+  case 'dmGetCollectionList':
+    $results = dmGetCollectionList();
+    break;
+  case 'allFeaturedItems':
+    $results = all_featured_items();
+    break;
+  case 'featuredItemsForCollection':
+    featured_items_for_collection($params['category']);
+    break;
+  default:
+    $results = array( 'error' => 'invalid operation requested: '.$_POST['command'] );
 }
 
 echo $json->encode($results);
