27f1077716f1e30dff8323c610c397e2f87c1aa6
Derek Kastner <dkastner@gmail.com>
2009-01-12 21:09:42 -0500

    Fixed syntax error

--- api/getCollectionsWithMetaData.php
+++ api/getCollectionsWithMetaData.php
@@ -3,14 +3,14 @@ function getCollectionsWithMetaData() {
   $collections = dmGetCollectionList();
   foreach($collections as &$item) {
     $image_settings = dmGetCollectionFullResInfo($item['alias'], $enabled, $public, $volprefix, $volsize, $displaysize, $archivesize);
-    $item['full_res_info'] = array({
+    $item['full_res_info'] = array(
       'enabled' => $enabled, 
       'public' => $public, 
       'volprefix' => $volprefix, 
       'volsize' => $volsize, 
       'displaysize' => $displaysize, 
       'archivesize' => $archivesize
-    });
+    );
   }
   return $collections;
 }
