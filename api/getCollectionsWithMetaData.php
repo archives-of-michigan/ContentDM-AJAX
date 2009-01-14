3691d62f9c73bebe1b8bb0facec17314254bd92c
Derek Kastner <dkastner@gmail.com>
2009-01-13 19:49:50 -0500

    Parsing out image width/height [#17]

--- api/getCollectionsWithMetaData.php
+++ api/getCollectionsWithMetaData.php
@@ -2,14 +2,21 @@
 function getCollectionsWithMetaData() {
   $collections = dmGetCollectionList();
   foreach($collections as &$item) {
-    $image_settings = dmGetCollectionFullResInfo($item['alias'], $enabled, $public, $volprefix, $volsize, $displaysize, $archivesize);
+    dmGetCollectionFullResInfo($item['alias'], $enabled, $public, $volprefix, $volsize, $displaysize, $archivesize);
+    
+    $archive_dimensions = '';
+    if($archivesize != '') {
+      $size = split('x',$archivesize);
+      $archive_dimensions = array('width' => $size[0], 'height' => $size[1]);
+    }
+    
     $item['full_res_info'] = array(
       'enabled' => $enabled, 
       'public' => $public, 
       'volprefix' => $volprefix, 
       'volsize' => $volsize, 
       'displaysize' => $displaysize, 
-      'archivesize' => $archivesize
+      'archivesize' => $archive_dimensions
     );
   }
   return $collections;
