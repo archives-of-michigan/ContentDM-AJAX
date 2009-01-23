7aadd638927515b3e29d403e66b5b069c3cd856c
Derek Kastner <dkastner@gmail.com>
2009-01-22 20:36:11 -0500

    featured_items_for_collection spec [#18]

---
+++ examples/cdm/featured_items_for_collection_spec.rb
@@ -0,0 +1,34 @@
+require File.join(File.dirname(__FILE__),'..','spec_helper')
+require 'curb'
+require 'json'
+require 'htmlentities'
+
+describe "featured_items_for_collection" do
+  before(:each) do
+    @params = {
+      'alias' => ['p4006coll2']
+    }
+  end
+  
+  def do_post
+    @results = execute_command('featured_items_for_collection', @params)
+  end
+  
+  it "should return the collection name" do
+    do_post
+    @results['name'].should == 'Governors of Michigan'
+  end
+  it "should return the collection image settings" do
+    do_post
+    @results['full_res_info'].should be_a_kind_of(Hash)
+    @results['full_res_info']['archivesize'].should == {"height"=>"2400", "width"=>"3200"}
+  end
+  it "should return [false] for an invalid alias" do
+    @params = {
+      'alias' => ['abcdef']
+    }
+    do_post
+    
+    @results.should == [false]
+  end
+end
