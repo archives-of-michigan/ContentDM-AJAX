aa92f7b10149b6a59cf84eb95006312d6bec9cae
Derek Kastner <dkastner@gmail.com>
2009-03-29 17:09:05 -0400

    Fixed featured items for collection

--- api/featured_items_for_collection.php
+++ api/featured_items_for_collection.php
@@ -12,17 +12,8 @@ function featured_items_for_collection($alias) {
   $items = dmQuery(array($alias), $params, $fields, array('Date'), 50, 1, $total);
   
   for($x = 0; $x < count($items); $x++) {
-    // $items[$x]['debug'] = var_export($items[$x]);
-    if(preg_match('/image/i',$items[$x]['formab']) > 0) {
-      $items[$x]['content_type'] = 'image';
-      $items[$x]['src'] = "http://haldigitalcollections.cdmhost.com/cgi-bin/thumbnail.exe?CISOROOT=".$items[$x]['collection']."&CISOPTR=".$items[$x]['pointer'];
-      $items[$x]['url'] = "http://haldigitalcollections.cdmhost.com/seeking_michigan/discover_item_viewer.php?CISOROOT=".$items[$x]['collection']."&CISOPTR=".$items[$x]['pointer']."&CISOBOX=1&REC=1";
-    } elseif(preg_match('/audio/i',$items[$x]['formab']) > 0) {
-      $items[$x]['content_type'] = 'audio';
-      $items[$x]['url'] = "http://haldigitalcollections.cdmhost.com/seeking_michigan/discover_item_viewer.php?CISOROOT=".$items[$x]['collection']."&CISOPTR=".$items[$x]['pointer']."&CISOBOX=1&REC=1";
-    } else {
-      $items[$x]['url'] = "http://haldigitalcollections.cdmhost.com/seeking_michigan/discover_item_viewer.php?CISOROOT=".$items[$x]['collection']."&CISOPTR=".$items[$x]['pointer']."&CISOBOX=1&REC=1";
-    }
+    $items[$x]['thumbnail'] = "http://haldigitalcollections.cdmhost.com/cgi-bin/thumbnail.exe?CISOROOT=".$items[$x]['collection']."&CISOPTR=".$items[$x]['pointer'];
+    $items[$x]['url'] = "http://haldigitalcollections.cdmhost.com/seeking_michigan/discover_item_viewer.php?CISOROOT=".$items[$x]['collection']."&CISOPTR=".$items[$x]['pointer'];
   }
   
   return $items;
