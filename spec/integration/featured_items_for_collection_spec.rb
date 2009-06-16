a4ca206c04b72e9a58ce9b607ec1596ba2f8b715
Derek Kastner <dkastner@gmail.com>
2009-06-15 21:30:08 -0400

    Test for featured items

--- spec/integration/featured_items_for_collection_spec.rb
+++ spec/integration/featured_items_for_collection_spec.rb
@@ -11,4 +11,13 @@ describe "featured_items_for_colelction" do
     
     response.headers['Content-Type'].should == 'application/json'
   end
+  
+  describe "specific collections" do
+    it "should work for p4006coll5" do
+      response = post_method('featured_items_for_collection', { 'params' => { 'alias' => '/p4006coll5' }.to_json })
+      
+      response.should be_success
+      response.body.should == ""
+    end
+  end
 end
