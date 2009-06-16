9a0f9c3bdaa210c1378114573e75fa765cb62d7f
Derek Kastner <dkastner@gmail.com>
2009-06-15 21:41:24 -0400

    config file

---
+++ cdm_ajax_config.php
@@ -0,0 +1,7 @@
+<?php
+
+class CDMConfiguration {
+  const root_path = '../..';
+  const uri_path = 'http://cdm9006.cdmhost.com/custom/govern';
+  const cgi_path = 'http://haldigitalcollections.cdmhost.com/cgi-bin';
+}
