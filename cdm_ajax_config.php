ebfa1c82f1fdeaaf52b9e36803ce0782889ec9e7
Archives <markharvey@mark-harveys-computer-3.local>
2009-12-14 13:49:25 -0500

    changed urls to seekingmichigan.cdmhost.com

--- cdm_ajax_config.php
+++ cdm_ajax_config.php
@@ -2,6 +2,6 @@
 
 class CDMConfiguration {
   const root_path = '..';
-  const uri_path = 'http://haldigitalcollections.cdmhost.com/seeking_michigan';
-  const cgi_path = 'http://haldigitalcollections.cdmhost.com/cgi-bin';
+  const uri_path = 'http://seekingmichigan.cdmhost.com/seeking_michigan';
+  const cgi_path = 'http://seekingmichigan.cdmhost.com/cgi-bin';
 }
